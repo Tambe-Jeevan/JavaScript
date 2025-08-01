8143765c738a71c1a108f639c5fbd5a6ab6a2592
Jeevan Tambe <tambejeevan816@gmail.com>
2025-08-02 00:07:13 +0530

    Add Some code on 24_For_Of Js File.

--- 24_for_of.js
+++ 24_for_of.js
@@ -6,16 +6,19 @@
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
-    //console.log(num);
+    // console.log(num);
 }
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+   if(greet === " "){
+      continue
+    }else{
+        //  console.log(`Each char is ${greet}`);
+    }
 }
 
 // Maps
-
 const map = new Map()
 map.set('IN', "India")
 map.set('USA', "United States of America")
@@ -26,15 +29,15 @@ map.set('IN', "India")
 // console.log(map);
 
 for (const [key, value] of map) {
-    console.log(key, ':-', value);
+    // console.log(key, ':-', value);
 }
 
-const myObject = {
+let myObject = {
     game1: 'NFS',
     game2: 'Spiderman'
 }
 
-// for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);
     
-// }
+}
