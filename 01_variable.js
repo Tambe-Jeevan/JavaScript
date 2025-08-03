6cb2a2b2abef478d7b1d72ce04d91104c16dfe02
Jeevan Tambe <tambejeevan816@gmail.com>
2025-08-03 20:25:52 +0530

    Add Javascript Practice Questions

--- 01_variable.js
+++ 01_variable.js
@@ -10,3 +10,20 @@ console.log(id);
 
 // dont use the var because of issue in block scope and function scope
 
+/* practice Questions
+True or False.
+JAVA is short for JavaScript.
+True
+False
+
+Who invented JavaScript?
+Tim Berners-Lee
+Brendan Eich
+Haakon Wium Lie
+
+
+What is NOT a correct syntax for writing output in JavaScript?
+window.alert()
+console.log()
+body.html()
+document.write()*/
